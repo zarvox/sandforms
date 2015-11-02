f85f620af3c5f7c8708e17e2092783b52ea217eb
Jack Singleton <git@jacksingleton.com>
2015-11-01 16:12:21 -0800

    add more comprehensive disallows and run inside Meteor.startup

--- server/csp.js
+++ server/csp.js
@@ -1,6 +1,16 @@
-BrowserPolicy.content.disallowInlineScripts();
-BrowserPolicy.content.disallowConnect();
+Meteor.startup(function() {
+  BrowserPolicy.content.disallowInlineScripts();
+  BrowserPolicy.content.disallowEval();
+  BrowserPolicy.content.disallowInlineStyles();
 
-var rootUrl = __meteor_runtime_config__.ROOT_URL;
-BrowserPolicy.content.allowConnectOrigin(rootUrl);
-BrowserPolicy.content.allowConnectOrigin(rootUrl.replace('http', 'ws'));
+  // Our app always runs in a sandstorm iframe
+  // TODO: restrict this to our origin minus the app
+  // specific subdomain?
+  BrowserPolicy.framing.allowAll();
+
+  // TODO: only allow connects to our own origin
+  //BrowserPolicy.content.disallowConnect();
+  //var rootUrl = __meteor_runtime_config__.ROOT_URL;
+  //BrowserPolicy.content.allowConnectOrigin(rootUrl);
+  //BrowserPolicy.content.allowConnectOrigin(rootUrl.replace('http', 'ws'));
+});
